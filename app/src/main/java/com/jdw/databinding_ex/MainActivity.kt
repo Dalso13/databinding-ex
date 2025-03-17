680fee3ab7600b44e659f97d671a80e4643df8c4
Dalso13 <flskwhdhk@gmail.com>
2025-03-17 18:29:49 +0900

    feat : datavinding 과 id 접근 비교

--- app/src/main/java/com/jdw/databinding_ex/MainActivity.kt
+++ app/src/main/java/com/jdw/databinding_ex/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.jdw.databinding_ex
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.jdw.databinding_ex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+
+        // 기존 방식
+//        val btn = findViewById<Button>(R.id.testButton)
+//        btn.setOnClickListener {
+//            Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
+//        }
+
+        // 데이터 바인딩 이용
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.testButton.setOnClickListener {
+            Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
         }
+
     }
 }
